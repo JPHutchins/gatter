e2df6aa1bb43641db7b87a91b1f4eeafa24f29f2
sparklerfish <julie.ellen.adams@gmail.com>
2022-03-04 22:49:53 -0800

    feat: add error dialogs in electron

--- frontend/electron/main.js
+++ frontend/electron/main.js
@@ -1,58 +1,48 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
 const isDev = require('electron-is-dev');
 const path = require('path');
 const { spawn } = require('child_process');
-const { fetch } = require("cross-fetch");
-
-let mainWindow;
+const { fetch } = require('cross-fetch');
 
 function createWindow() {
-    const child = spawn(process.env.PYTHON_ALIAS, ["../backend/main.py"]);
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        show: false,
+    });
+    const child = spawn(process.env.PYTHON_ALIAS, ['../backend/main.py']);
     child.stdout.on('data', (out) => {
         console.log(out.toString());
     });
-    child.stderr.on('data', (err) => {
+    child.stderr.on('data', async(err) => {
         console.log(err.toString());
         if (err.toString().includes('Application startup complete.')) {
-            console.log("Server started.")
-            fetch("http://localhost:8000/tests/hello_world", {
-                mode: "no-cors",
-                method: "get",
-                url: "http://localhost:8000",
-            })
-                .then(response => {
-                    if (!response.ok) {
-                        console.error("ERROR!");
-                        throw new Error("Backend did not respond at tests/hello_world");
-                    }
-                    return response;
-                })
-                .then(response => response.json())
-                .then(data => {
-                    if (data.msg !== 'Hello world!') {
-                        throw new Error("Backend did not respond Hello world!");
-                    }
-                })
+            console.log('Server started.');
+            const response = await fetch('http://localhost:8000/tests/hello_world');
+            if (!response.ok) {
+                dialog.showMessageBoxSync(mainWindow, { message: 'Backend did not respond at tests/hello_world' });
+                app.exit(1);
+            }
+            const data = await response.json();
+            if (data.msg !== 'Hello world!') {
+                dialog.showMessageBoxSync(mainWindow, { message: 'Backend did not respond Hello world!' });
+                app.exit(1);
+            }
         }
     });
     child.on('close', (code) => {
         console.log(`child process exited with code ${code}`);
     });
 
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        show: false
-    });
     const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`;
 
     mainWindow.loadURL(startURL);
 
     mainWindow.once('ready-to-show', () => mainWindow.show());
     mainWindow.on('closed', () => {
-        mainWindow = null;
         child.kill('SIGINT');
-        console.log("Backend closed.")
+        console.log('Backend closed.');
     });
 }
+
 app.on('ready', createWindow);
