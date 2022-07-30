f4cbcd9fb2a33ada5bae9ea3602a97c369f6c0c0
J.P. Hutchins <jphutchins@gmail.com>
2022-07-29 21:30:41 -0700

    feat: being removing environment variable dependency

--- backend/gatterserver/models/const.py
+++ backend/gatterserver/models/const.py
@@ -1,10 +1,10 @@
 """Constants and environment variables for models."""
 
-import os
+# import os
 
-API_CMD_ADD_PATH = os.environ["API_CMD_ADD_PATH"]
-API_CMD_DEL_PATH = os.environ["API_CMD_DEL_PATH"]
-API_CMD_START_STREAM_PATH = os.environ["API_CMD_START_STREAM_PATH"]
-RAMP_EMITTER_TYPE = os.environ["RAMP_EMITTER_TYPE"]
-BLE_EMITTER_TYPE = os.environ["BLE_EMITTER_TYPE"]
-SERIAL_EMITTER_TYPE = os.environ["SERIAL_EMITTER_TYPE"]
+API_CMD_ADD_PATH = "/api/cmd/add"  # os.environ["API_CMD_ADD_PATH"]
+API_CMD_DEL_PATH = "/api/cmd/del"  # os.environ["API_CMD_DEL_PATH"]
+API_CMD_START_STREAM_PATH = "/api/cmd/stream/start"  # os.environ["API_CMD_START_STREAM_PATH"]
+RAMP_EMITTER_TYPE = "ramp"  # os.environ["RAMP_EMITTER_TYPE"]
+BLE_EMITTER_TYPE = "ble"  # os.environ["BLE_EMITTER_TYPE"]
+SERIAL_EMITTER_TYPE = "serial"  # os.environ["SERIAL_EMITTER_TYPE"]
