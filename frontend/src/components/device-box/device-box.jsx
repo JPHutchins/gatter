55cc88ab8ab8160a3d86ce96bf2a297482e583f6
J.P. Hutchins <jphutchins@gmail.com>
2022-08-03 19:55:00 -0700

    feat: selectable byte parsing

--- frontend/src/components/device-box/device-box.jsx
+++ frontend/src/components/device-box/device-box.jsx
@@ -16,9 +16,7 @@ const properties = {
         if (response.status === 200) {
             const readResponse = await response.arrayBuffer();
             const buffer = new Uint8Array(readResponse);
-            const array = Array.from(buffer).map((x) => x.toString(16));
-            const string = array.join(' ');
-            setValue(string);
+            setValue(buffer);
         }
         else {
             console.error('Read failed.');
@@ -42,17 +40,48 @@ const properties = {
 }
 
 const Property = ({ property, characteristic, deviceId }) => {
-    const [value, setValue] = useState('');
+    const [value, setValue] = useState(new Uint8Array([]));
+    const [parser, setParser] = useState("hex");
+
     const streamId = (characteristic?.streamId?.deviceId << 8) | characteristic?.streamId?.channelId;
     const stream = useContextSelector(store, ({ state }) => state.streams?.[streamId]) ?? {};
-    const payload = stream.payload ? Array.from(stream.payload) : [];
-    const array = payload.map((x) => x.toString(16));
-    const notifyString = array.join(' ');
+
+    const payload = stream.payload ? Array.from(stream.payload) : Array.from(value);
     const action = properties[property];
+
+    let parsed = '';
+    switch (parser) {
+        case "hex":
+            parsed = payload.map((x) => x < 0x10 ? "0" + x.toString(16) : x.toString(16)).join(' ');
+            break;
+        case "dec":
+            parsed = payload.join(', ');
+            break;
+        case "str":
+            parsed = String.fromCharCode(...payload);
+            break;
+        default:
+            throw Error(`Bad value for parse: ${parser}`);
+    }
+
+    const id = property + characteristic.uuid + deviceId.toString(10);
+
     return ( 
         <li className="property">
             <button onClick={() => action(deviceId, characteristic, setValue)}>{property}</button>
-            <div>{value || notifyString}&nbsp;</div>
+            
+            <div className="format-radios">
+                <input type="radio" id={"hex" + id} name={"parser" + id} value="hex" onChange={() => setParser("hex")} checked={parser === "hex"} />
+                <label for={"hex" + id}>hex</label>
+
+                <input type="radio" id={"dec" + id} name={"parser" + id} value="dec" onChange={() => setParser("dec")}/>
+                <label for={"dec" + id}>dec</label>
+
+                <input type="radio" id={"str" + id} name={"parser" + id} value="str" onChange={() => setParser("str")}/>
+                <label for={"str" + id}>str</label>
+            </div>
+            
+            <div className="property-text-box">{parsed}&nbsp;</div>
         </li>
     )
 };
