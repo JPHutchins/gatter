36ff11c30524a22bf60a0f63ddc0d5fa0a0f314f
J.P. Hutchins <jphutchins@gmail.com>
2022-08-15 23:46:58 -0700

    wip: minimal implementation of log stream controller

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,6 @@ function App() {
     useEffect(() => {
         const ws = new WebSocket('ws://localhost:8000/api/ws/streams');
         const ws2 = new WebSocket('ws://localhost:8000/api/ws/blediscovery');
-        const ws3 = new WebSocket('ws://localhost:8000/api/ws/log');
 
         ws.onmessage = async(e) => {
             const buffer = await e.data.arrayBuffer();
@@ -41,9 +40,6 @@ function App() {
             });
         };
 
-        ws3.onmessage = async (message) => {
-            console.log(message.data);
-        }
     }, []);
 
     return <Board />;
